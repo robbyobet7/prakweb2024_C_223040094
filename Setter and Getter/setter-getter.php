287947039c40cc37b8b54e1027e25ddfe0df7533
robbyobet7 <muhamadrobby24@gmail.com>
2024-10-20 12:27:47 +0700

    OOP DASAR pada PHP #9 - Visibility

--- Setter and Getter/setter-getter.php
+++ Setter and Getter/setter-getter.php
@@ -1,7 +1,7 @@
 <?php 
 
 class Produk {
-    public  $judul,
+    private  $judul,
             $penulis,
             $penerbit;
 
@@ -14,6 +14,10 @@ class Produk {
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
+    }
+
+    public function getJudul() {
+        return $this->judul;
     }
 
     public function getHarga() {
@@ -89,3 +93,4 @@ echo "<hr>";
 
 $produk2->setDiskon(50);
 echo $produk2->getHarga();
+echo "<hr>";
