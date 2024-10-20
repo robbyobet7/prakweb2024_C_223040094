3f0b7e5ec772c1c36851c948c6ae6568ac9df34b
robbyobet7 <muhamadrobby24@gmail.com>
2024-10-20 12:54:30 +0700

    OOP DASAR pada PHP #10 - Setter dan Getter

--- Setter and Getter/setter-getter.php
+++ Setter and Getter/setter-getter.php
@@ -3,11 +3,9 @@
 class Produk {
     private  $judul,
             $penulis,
-            $penerbit;
-
-    protected $diskon = 0;
-
-    private $harga;
+            $penerbit,
+            $harga,
+            $diskon = 0;
 
     public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
         $this->judul = $judul;
@@ -16,12 +14,44 @@ class Produk {
         $this->harga = $harga;
     }
 
+    public function setJudul($judul) {
+        $this->judul = $judul;
+    }
+
     public function getJudul() {
         return $this->judul;
+    }
+ 
+    public function setPenulis($penulis) {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenulis() {
+        return $this->penulis;
+    }
+    
+    public function setPenerbit($penerbit) {
+        return $this->penerbit;
+    }
+
+    public function getPenerbit() {
+        return $this->penerbit;
+    }
+    
+    public function setHarga($harga) {
+        return $this->harga = $harga;
     }
 
     public function getHarga() {
         return $this->harga - ( $this->harga * $this->diskon / 100);
+    }
+
+    public function setDiskon($diskon) {
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon() {
+        $this->diskon;
     }
 
     public function getLabel() {
@@ -33,7 +63,6 @@ class Produk {
         return $str;
   }
 }
-
 
 
 class Komik extends Produk {
@@ -60,10 +89,6 @@ class Game extends Produk {
         parent:: __construct($judul, $penulis, $penerbit, $harga);
         
         $this->waktuMain = $waktuMain;
-    }
-
-    public function setDiskon($diskon) {
-        $this->diskon = $diskon;
     }
 
     public function getInfoProduk() {
@@ -94,3 +119,6 @@ echo "<hr>";
 $produk2->setDiskon(50);
 echo $produk2->getHarga();
 echo "<hr>";
+
+$produk1->setPenulis("M. Robby A");
+echo $produk1->getPenulis();
